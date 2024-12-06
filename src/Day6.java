40690561fef92a31a4f311477ef94bea5f8502ae
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-12-06 13:11:56 +0100

    Added day 6 part 2

--- src/Day6.java
+++ src/Day6.java
@@ -1,5 +1,5 @@
 import java.io.FileNotFoundException;
-import java.util.List;
+import java.util.*;
 
 public class Day6 extends Day {
     private final String filePath = "data/day6/input.txt";
@@ -16,6 +16,37 @@ public class Day6 extends Day {
         }
     }
 
+    @Override
+    protected void part2() {
+        try {
+            List<String> file = getFile(filePath);
+            int height = file.size();
+            int width = file.getFirst().length();
+
+            int count = 0;
+
+            for (int y = 0; y < height; y++) {  // infinite loop on 20 confirmed
+                for (int x = 0; x < width; x++) {   // infinite loop on 68 confirmed
+                    Board board = new Board(file);
+                    Vector pos = new Vector(x, y);
+                    if (board.obstacleAt(pos)) {
+                        continue;
+                    }
+
+                    board.placeObstacle(pos);
+
+                    if (board.willLoop()) {
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println("Visited: " + count);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+    }
+
     private enum Item {
         Nothing, Obstacle, Visited
     }
@@ -23,12 +54,20 @@ public class Day6 extends Day {
     private static class Board {
 
         private final Item[][] items;
+        private Vector guardPos;
+        private Vector guardDirection;
         private final Guard guard;
+        private final Map<Vector, Set<Vector>> visited;
+
+        private Board(Item[][] items, Vector guardPos, Vector guardDirection, Map<Vector, Set<Vector>> visited) {
+            this.items = items;
+            guard = new Guard(guardPos, guardDirection, this);
+            this.visited = visited;
+        }
 
         Board(List<String> map) {
-            Vector guardPos = null;
-            Vector guardDirection = null;
             items = new Item[map.size()][map.getFirst().length()];
+            visited = new HashMap<>();
 
             for (int i = 0; i < items.length; i++) {
                 String str = map.get(i);
@@ -52,6 +91,14 @@ public class Day6 extends Day {
             guard = new Guard(guardPos, guardDirection, this);
         }
 
+        public Board clone() {
+            return new Board(items.clone(), guardPos, guardDirection, new HashMap<>());
+        }
+
+        public void placeObstacle(Vector position) {
+            items[position.y][position.x] = Item.Obstacle;
+        }
+
         public int startSimulation() {
             while (!outOfBoard(guard.getPosition())) {
                 if (!guard.moveForward()) {
@@ -73,6 +120,26 @@ public class Day6 extends Day {
             return visited;
         }
 
+        public boolean willLoop() {
+            while (!outOfBoard(guard.getPosition())) {
+                if (!guard.moveForward()) {
+                    guard.rotate90();
+                    // rather than checking if the next turn has been made already
+                    // check if the current position and direction has already been moved before
+
+                    Vector position = guard.getPosition();
+                    Vector direction = guard.getDirection();
+
+                    if (visited.containsKey(position)) {
+                        if (visited.get(position).contains(direction)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
         public boolean obstacleAt(Vector position) {
             if (outOfBoard(position)) {
                 return false;
@@ -82,6 +149,14 @@ public class Day6 extends Day {
         }
 
         public void markVisited(Vector position) {
+            if (visited.containsKey(position)) {
+                Set<Vector> directions = visited.get(position);
+                directions.add(guardDirection);
+            } else {
+                Set<Vector> directions = new HashSet<>();
+                directions.add(guard.getDirection());
+                visited.put(position, directions);
+            }
             items[position.y][position.x] = Item.Visited;
         }
 
@@ -127,6 +202,10 @@ public class Day6 extends Day {
         public Vector getPosition() {
             return position;
         }
+
+        public Vector getDirection() {
+            return direction;
+        }
     }
 
     private static class Vector {
@@ -139,6 +218,18 @@ public class Day6 extends Day {
 
         public Vector add(Vector second) {
             return new Vector(this.x + second.x, this.y + second.y);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Vector vector = (Vector) o;
+            return x == vector.x && y == vector.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
     }
 }
